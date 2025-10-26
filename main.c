892ee50b3a94a3bcc688b677b1e7a5862eeac738
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-10-26 13:10:53 +0300

    fixed: CPU usage used to be very high due to the while loop, but adding a poll fixed the issue

--- main.c
+++ main.c
@@ -34,6 +34,8 @@ int main(void)
 	struct udev *udev;
 	struct s_config config = {0};
 	char config_path[256];
+	struct pollfd fds;
+	int libinput_fd;
 
 	// snprintf(config_path, sizeof(config_path), "%s/.config/jestapp/config.ini", getenv("HOME"));
 	snprintf(config_path, sizeof(config_path), "config.ini");
@@ -69,79 +71,100 @@ int main(void)
 		udev_unref(udev);
 		return (1);
 	}
+	libinput_fd = libinput_get_fd(li);
+	if (libinput_fd < 0)
+	{
+		fprintf(stderr, "error : libinputgetfd");
+		libinput_unref(li);
+		udev_unref(udev);
+		return 1;
+	}
 	printf("sucess : added a touchpad \n");
 	while (1)
 	{
-		libinput_dispatch(li);
-		while ((event = libinput_get_event(li)))
+		fds.fd = libinput_fd;
+		fds.events = POLLIN;
+		fds.revents = 0;
+
+		int ret = poll(&fds, 1, -1);
+		if (ret < 0)
 		{
-			type = libinput_event_get_type(event);
-			switch (type)
+			fprintf(stderr, "error : poll");
+			continue;
+		}
+		if (fds.revents && POLLIN)
+		{
+			libinput_dispatch(li);
+			while ((event = libinput_get_event(li)))
 			{
-			case LIBINPUT_EVENT_GESTURE_SWIPE_BEGIN:
-				printf("start a gesture \n");
-				total_dx = 0.0;
-				total_dy = 0.0;
-				break;
-			case LIBINPUT_EVENT_GESTURE_SWIPE_UPDATE:
-				gesture_event = libinput_event_get_gesture_event(event);
-				total_dx += libinput_event_gesture_get_dx(gesture_event);
-				total_dy += libinput_event_gesture_get_dy(gesture_event);
-				break;
-			case LIBINPUT_EVENT_GESTURE_SWIPE_END:
-				gesture_event = libinput_event_get_gesture_event(event);
-				finger_count = libinput_event_gesture_get_finger_count(gesture_event);
-				printf("finger count : %d \n", finger_count);
-				printf("total_dx: %.2f\ntotal_dy: %.2f\n", total_dx, total_dy);
-				if (ft_fabs(total_dy) > ft_fabs(total_dx))
+				type = libinput_event_get_type(event);
+				switch (type)
 				{
-					if (total_dy < 0)
+				case LIBINPUT_EVENT_GESTURE_SWIPE_BEGIN:
+					printf("start a gesture \n");
+					total_dx = 0.0;
+					total_dy = 0.0;
+					break;
+				case LIBINPUT_EVENT_GESTURE_SWIPE_UPDATE:
+					gesture_event = libinput_event_get_gesture_event(event);
+					total_dx += libinput_event_gesture_get_dx(gesture_event);
+					total_dy += libinput_event_gesture_get_dy(gesture_event);
+					break;
+				case LIBINPUT_EVENT_GESTURE_SWIPE_END:
+					gesture_event = libinput_event_get_gesture_event(event);
+					finger_count = libinput_event_gesture_get_finger_count(gesture_event);
+					printf("finger count : %d \n", finger_count);
+					printf("total_dx: %.2f\ntotal_dy: %.2f\n", total_dx, total_dy);
+					if (ft_fabs(total_dy) > ft_fabs(total_dx))
 					{
-						printf("fingers up \n");
-						if (finger_count == 3)
-							run_command(config.swipe_up_3);
-						else if (finger_count == 4)
-							run_command(config.swipe_up_4);
+						if (total_dy < 0)
+						{
+							printf("fingers up \n");
+							if (finger_count == 3)
+								run_command(config.swipe_up_3);
+							else if (finger_count == 4)
+								run_command(config.swipe_up_4);
+						}
+						else
+						{
+							printf("fingers down \n");
+							if (finger_count == 3)
+								run_command(config.swipe_down_3);
+							else if (finger_count == 4)
+								run_command(config.swipe_down_4);
+						}
 					}
 					else
 					{
-						printf("fingers down \n");
-						if (finger_count == 3)
-							run_command(config.swipe_down_3);
-						else if (finger_count == 4)
-							run_command(config.swipe_down_4);
+						if (total_dx < 0)
+						{
+							printf("fingers left \n");
+							if (finger_count == 3)
+								run_command(config.swipe_left_3);
+							else if (finger_count == 4)
+								run_command(config.swipe_left_4);
+						}
+						else
+						{
+							printf("fingers right \n");
+							if (finger_count == 3)
+								run_command(config.swipe_right_3);
+							else if (finger_count == 4)
+								run_command(config.swipe_right_4);
+						}
 					}
+					printf("end a gesture \n");
+					break;
+				case LIBINPUT_EVENT_GESTURE_PINCH_BEGIN:
+					printf("start a pinch \n");
+					break;
+				case LIBINPUT_EVENT_GESTURE_PINCH_END:
+					printf("end a pinch \n");
+				default:
+					break;
 				}
-				else
-				{
-					if (total_dx < 0)
-					{
-						printf("fingers left \n");
-						if (finger_count == 3)
-							run_command(config.swipe_left_3);
-						else if (finger_count == 4)
-							run_command(config.swipe_left_4);
-					}
-					else
-					{
-						printf("fingers right \n");
-						if (finger_count == 3)
-							run_command(config.swipe_right_3);
-						else if (finger_count == 4)
-							run_command(config.swipe_right_4);
-					}
-				}
-				printf("end a gesture \n");
-				break;
-			case LIBINPUT_EVENT_GESTURE_PINCH_BEGIN:
-				printf("start a pinch \n");
-				break;
-			case LIBINPUT_EVENT_GESTURE_PINCH_END:
-				printf("end a pinch \n");
-			default:
-				break;
+				libinput_event_destroy(event);
 			}
-			libinput_event_destroy(event);
 		}
 	}
 	free(config.swipe_up_3);
